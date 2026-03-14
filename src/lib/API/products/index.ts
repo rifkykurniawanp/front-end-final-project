fa6e06930df1ae3b91aacfcafedbdc3095a96fdb
Rifky Kurniawan Putra <krifky14@gmail.com>
2026-03-14 17:18:34 +0700

    back index

--- src/lib/API/products/index.ts
+++ src/lib/API/products/index.ts
@@ -1,15 +1,4 @@
-// src/lib/API/products/index.ts
-
-// import spesifik dari masing-masing file
-// import { productOrdersApi } from './product-orders.api'; // kalau nanti dibutuhkan
-import { productOrdersApi } from './product-order-items.api';
-import { productReviewsApi } from './product-reviews.api';
-import { productsApi } from './products.api';
-
-// export spesifik
-export {
-  // productOrdersApi, // jangan di-export kalau tidak digunakan
-  productOrdersApi,
-  productReviewsApi,
-  productsApi
-};
+export * from "./product-order-items.api"
+// export * from "./product-orders.api"
+export * from "./product-reviews.api"
+export * from "./products.api"
